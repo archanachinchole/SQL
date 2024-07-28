29aae833fe000c1e4cf130c13824e7e1d01801c2
Archana Chinchole <archanachinchole6@gmail.com>
2024-07-28 14:48:36 +0530

    sql

--- MySQL1.session.sql
+++ MySQL1.session.sql
@@ -1,13 +1,16 @@
-SELECT
-      child.member as child_id,
-      child.name as child_name,
-      child.personality as child_personality 
-from
-child
-join       
-parents
-ON
-child.parents_member = parents.member
-where 
-parents.age >=50;
 
+SELECT 
+    parents.member AS parents_id,
+    parents.name AS parent_name,
+    parents.personality AS parent_personality,
+    parents.age AS parent_age,
+    child.name as child_name
+FROM 
+    parents   
+JOIN 
+    child
+ON 
+    parents.member = child.parents_member
+ORDER BY 
+    parents.age DESC
+LIMIT 1;
--- SQL5.sql
+++ SQL5.sql
@@ -103,3 +103,21 @@ ON
 child.parents_member = parents.member
 where 
 parents.age >=50;
+
+
+#highest age of child parents
+SELECT 
+    parents.member AS parents_id,
+    parents.name AS parent_name,
+    parents.personality AS parent_personality,
+    parents.age AS parent_age,
+    child.name as child_name
+FROM 
+    parents   
+JOIN 
+    child
+ON 
+    parents.member = child.parents_member
+ORDER BY 
+    parents.age DESC
+LIMIT 1;
