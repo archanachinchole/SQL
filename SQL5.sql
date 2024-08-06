05148d801513ed5fa880fbcda3d112b283404641
Archana Chinchole <archanachinchole6@gmail.com>
2024-08-06 12:44:00 +0530

    sql

--- SQL5.sql
+++ SQL5.sql
@@ -134,3 +134,13 @@ insert into parents(member, name,personality,age)values
 
 
 select * from parents;
+
+##add age column in child table
+
+alter table child add column age varchar(50);
+
+update child set age=12 where member=1;
+update child set age=15 where member=2;
+update child set age=10 where member=3;
+update child set age=15 where member=4;
+update child set age=19 where member=5;
